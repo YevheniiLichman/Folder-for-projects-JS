1b058dd39b1c874fe95491b088f3b893db7317cf
YevheniiLichman <zhekalichm4n@icloud.com>
2022-02-22 16:42:14 +0200

    add

--- forrest.js
+++ forrest.js
@@ -1,16 +1,11 @@
 window.onload = function () {
-
     var text1 = document.getElementById("massBody");
-
     var button = document.getElementById("mainButton");
-
     var text2 = document.getElementById("heightBody");
-
     var result = document.getElementById("result__item");
-
     button.onclick = function (event) {
         var mass = text1.value;
-        var height = text2.value;
+        var height = text2.value / 100;
         normal = mass / (height * height)
         result.innerHTML = Math.round(normal)
         if (normal < 25) {
